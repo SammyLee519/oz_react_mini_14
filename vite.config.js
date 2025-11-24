262fa45633f11c9c73f6d64e2d182d44ef3f74c9
LeeSaemMool <sammul95@gmail.com>
2025-11-24 23:21:28 +0900

    fix: vite alias 경로 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src",
-      "@components": "/src/components",
-      "@api": "/src/api",
-      "@hook": "/src/hook",
-      "@pages": "/src/pages",
-      "@CommonStyle": "/src/components/CommonStyle",
-      "@context": "/src/context",
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
     },
   },
 });
